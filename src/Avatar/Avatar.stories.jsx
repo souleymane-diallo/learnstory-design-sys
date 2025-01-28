85d14bf2f0581af3c6f3f6a4cf98e4557c82f522
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2025-01-28 13:45:13 +0100

    relecture chapter 3

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -3,6 +3,23 @@ import { Avatar } from './Avatar';
 export default {
   title: 'Design System/Avatar',
   component: Avatar,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['tiny', 'small', 'medium', 'large'],
+    },
+  },
+};
+
+export const Controls = {
+  args: {
+    loading: false,
+    size: 'tiny',
+    username: 'Dominic Sonah',
+    src: 'https://avatars.githubusercontent.com/u/263385',
+  },
 };
 
 export const Standard = {
